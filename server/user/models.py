9b40efe3ed08aff925245c20b0d7443e73f3f51e
heejae <jhj967878@naver.com>
2021-11-17 15:55:35 +0900

    feat : user 테이블 생성

---
+++ server/user/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=256)
+    nickname = models.CharField(max_length=50)
+    user_type = models.IntegerField()
+    description = models.CharField(max_length=256)
+    image = models.CharField(max_length=100) # image url
+    
+    def __str__(self):
+        return self.email + "#" + str(self.user_type)
