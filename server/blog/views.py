c41f1c102aa87d2193dbd8fb235a98815e668d29
heejae <jhj967878@naver.com>
2021-11-18 18:08:14 +0900

    feat : 커뮤니티 기능 밑작업

--- server/blog/views.py
+++ server/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from django.conf import settings
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Blog
+import json
 # Create your views here.
+
+@api_view(['GET'])
+def get_blog(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk = blog_id)
+    
+    is_author = False
+    
+    response_data = {
+        'author': 'a',
+        'title': blog_detail.title,
+        'content': blog_detail.content,
+        'view': blog_detail.view,
+        'is_author': is_author
+    }
