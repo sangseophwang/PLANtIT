c4e09da2d65dc4c01a19dcf555dd325e7b4f913a
JinKyung Kim <kimradium@naver.com>
2021-11-21 17:52:36 +0900

    feat: 모달 창 수정, 서비스 소개 페이지 틀 제작, 더미 파일 여러개 추가

--- client/src/Routes/Introduction.tsx
+++ client/src/Routes/Introduction.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navigation from '../Components/Common/Navigation';
+import Banner from '../Components/Introduction/Banner';
+import IntroContent from '../Components/Introduction/IntroContent';
 
 export default function Introduction(): JSX.Element {
   return (
     <div>
       <Navigation />
+      <Banner />
+      <IntroContent />
     </div>
   );
 }
