831f6d768e847a1018604d0d2307426e06c20a7d
heejae <jhj967878@naver.com>
2021-11-21 17:52:45 +0900

    feat : API 매핑

--- server/user/urls.py
+++ server/user/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.login, name='user-login'),
+    path('register/', views.register, name='user-register'),
+    path('deregister/', views.deregister, name='user-deregister'),
+    path('update/', views.update, name='user-update'),
+    path('mypage/', views.mypage, name='user-mypage')
 ]
