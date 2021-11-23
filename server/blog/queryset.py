8a062cac312c908ba4bca9200e6301129ae9fd40
heejae <jhj967878@naver.com>
2021-11-23 14:43:53 +0900

    feat : 커뮤니티 get, post API

---
+++ server/blog/queryset.py
@@ -0,0 +1,41 @@
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Blog
+
+def find_blog_by_id(blog_id):
+    try:
+        blog = Blog.objects.get(pk=blog_id)
+    except ObjectDoesNotExist:
+        return False
+    else:
+        return blog
+    
+def create_blog(user, title, content, thumbnail):
+    new_blog = Blog(user=user, title=title, content=content, thumbnail=thumbnail, view=0)
+    try:
+        new_blog.save()
+    except:
+        return False
+    else:
+        return new_blog
+
+def update_blog_content(blog_id, content):
+    target_blog = find_blog_by_id(blog_id)
+    try:
+        target_blog.content = content
+        target_blog.save()
+    except:
+        return False
+    else:
+        return True
+    
+def update_blog(blog_id, title, content, thumbnail):
+    target_blog = find_blog_by_id(blog_id)
+    try:
+        target_blog.title = title
+        target_blog.content = content
+        target_blog.thumbnail = thumbnail
+        target_blog.save()
+    except:
+        return False
+    else:
+        return True
--- server/blog/views.py
+++ server/blog/views.py
@@ -1,32 +1,54 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from .models import Blog
-from user.views import validate_token
+from .queryset import find_blog_by_id, create_blog, update_blog_content, update_blog
+from user.queryset import find_user_by_id
+from common.token import validate_token
+from common.s3 import upload_blog_image
 import json
 # Create your views here.
 
-@api_view(['GET', 'PATCH', 'DELETE'])
-def blog(request, blog_id):
-    blog_detail = get_object_or_404(Blog, pk = blog_id)
+@api_view(['GET'])
+def get_blog(request, blog_id):
+    blog_detail = find_blog_by_id(blog_id)
+    
+    if not blog_detail:
+        return Response(data="Not Found Blog")
     
     is_author = False
+    access_token = request.META['HTTP_AUTHORIZATION']
+    token_validation = validate_token(access_token)
+    if token_validation.status_code == 200:
+        if token_validation.data['user_id'] == blog_detail.user.id:
+            is_author = True
     
     response_data = {
-        'author': 'a',
+        'author': blog_detail.user.nickname,
         'title': blog_detail.title,
         'content': blog_detail.content,
         'view': blog_detail.view,
         'is_author': is_author
     }
+    response_data = json.dumps(response_data)
+    return Response(data=response_data, status=200)
     
 @api_view(['POST'])
 def post_blog(request):
     access_token = request.META['HTTP_AUTHORIZATION']
     
     token_validation = validate_token(access_token)
-    if not token_validation == True:
+    if not token_validation.status_code == 200:
         return token_validation
+
+    user_id = token_validation.data['user_id']
+    user = find_user_by_id(user_id)
+    request_body = json.loads(request.body)
+    title = request_body['title']
+    content = request_body['content']
+    
+    create_blog(user=user, title=title, content='-', thumbnail='-')
     
     return Response(data='Post Success', status=200)
