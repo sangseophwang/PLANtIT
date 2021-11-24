14611eb81625deb0641ab927d1c78e6077eed561
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-11-24 16:29:55 +0900

    Fix: README.md 업데이트

--- client/src/Components/App.tsx
+++ client/src/Components/App.tsx
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 import React from 'react';
 import 'Components/App.scss';
 import 'Components/Common/scss/_reset.scss';
 import AppRouter from 'Components/Router';
-=======
-import './App.scss';
-import './Common/scss/_reset.scss';
-import AppRouter from './Router';
->>>>>>> 7e117cc7263b01acb3ce7fb5b63f7459b729c46f
 
 function App() {
   return (
