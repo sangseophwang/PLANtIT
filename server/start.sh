e22f8a78bf0ac82e13200b952f226e200935ac08
heejae <jhj967878@naver.com>
2021-11-24 16:28:03 +0900

    fix : LF, CRLF 버그 수정

--- server/start.sh
+++ server/start.sh
@@ -1,3 +1,1 @@
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py runserver 0.0.0.0:8000
