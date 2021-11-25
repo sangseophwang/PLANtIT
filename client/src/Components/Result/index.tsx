bd6c8fcfebf7109a7fe9821348ece3c98d5a745b
JinKyung Kim <kimradium@naver.com>
2021-11-25 16:24:38 +0900

    feat: 결과 페이지의 첫번째, 두번째 화면 틀 제작

--- client/src/Components/Result/index.tsx
+++ client/src/Components/Result/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navigation from 'Components/Common/Navigation';
 import 'Components/Result/scss/Result.scss';
-import SwiperCore, { Mousewheel, Pagination } from 'swiper';
+import SwiperCore, { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper.scss';
 import 'swiper/components/pagination/pagination.scss';
-import Test from './test';
 import Last from './Last';
 import Rfirst from 'Components/Result/Rfirst';
+import Rsecond from 'Components/Result/Rsecond';
+import Rthird from 'Components/Result/Rthird';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-SwiperCore.use([Mousewheel, Pagination]);
+SwiperCore.use([Pagination]);
 
 const bullet = [
   '- 질병명',
@@ -19,6 +22,47 @@ const bullet = [
 ];
 
 export default function Result(): JSX.Element {
+  // API로 부터 질병 진단 정보를 받아오는 Axios GET 코드.
+
+  // const [result, setResult] = useState([] as any);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+
+  // // API로 부터 정보를 받아옴.
+  // const GetResultAPI = async () => {
+  //   // 요청이 시작 할 때 초기화
+  //   setError(null);
+  //   setResult(null);
+  //   setLoading(true);
+  //   const ResultResponse = await axios
+  //     .get(`http://localhost:8000/api/아직 미정`)
+  //     .then(response => {
+  //       setResult(response.data);
+  //     })
+  //     .catch(e => {
+  //       setError(e);
+  //     });
+  //   setLoading(false);
+  //   return ResultResponse;
+  // };
+
+  // useEffect(() => {
+  //   GetResultAPI();
+  // }, []);
+
+  // if (loading)
+  //   return <div className="Notice__Container">잠시만 기다려 주세요</div>;
+  // if (error)
+  //   return (
+  //     <>
+  //       <div className="Notice__Container">API 에러가 발생했습니다</div>
+  //       <Link to="/Analysis" className="Button__Home">
+  //         뒤로가기
+  //       </Link>
+  //     </>
+  //   );
+  // if (!result) return null as any;
+
   return (
     <div className="Result__Container">
       <Navigation />
@@ -28,13 +72,12 @@ export default function Result(): JSX.Element {
         slidesPerView={1}
         spaceBetween={30}
         speed={1000}
-        mousewheel={true}
-        onReachEnd={swiper => {
-          swiper.mousewheel.disable();
-        }}
-        onSlideChangeTransitionStart={swiper => {
-          swiper.mousewheel.enable();
-        }}
+        // onReachEnd={swiper => {
+        //   swiper.mousewheel.disable();
+        // }}
+        // onSlideChangeTransitionStart={swiper => {
+        //   swiper.mousewheel.enable();
+        // }}
         pagination={{
           clickable: true,
           renderBullet: function (index, className) {
@@ -53,10 +96,10 @@ export default function Result(): JSX.Element {
           <Rfirst />
         </SwiperSlide>
         <SwiperSlide>
-          <Test />
+          <Rsecond />
         </SwiperSlide>
         <SwiperSlide>
-          <Test />
+          <Rthird />
         </SwiperSlide>
         <SwiperSlide>
           <Last />
--- client/src/Components/Result/test.tsx
+++
@@ -1,9 +0,0 @@
-import './scss/test.scss';
-
-export default function Test(): JSX.Element {
-  return (
-    <section className="Test__Container">
-      <h1>첫번째 페이지입니다.</h1>
-    </section>
-  );
-}
