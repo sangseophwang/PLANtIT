eabf4cbcc98b9141c2918b8c84fb9ff46d9ad065
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-11-28 10:46:31 +0900

    fix : 외부 DB 연동 및 테스트 코드 수정

--- server/time_log.py
+++ server/time_log.py
@@ -5,6 +5,6 @@ def logging_time(original_fn):
         start_time = time.time()
         result = original_fn(*args, **kwargs)
         end_time = time.time()
-        print("WorkingTime[{}]: {} sec".format(original_fn.__name__, end_time-start_time))
+        print("완료 시간[{}]: {} sec".format(original_fn.__name__, end_time-start_time))
         return result
     return wrapper_fn
