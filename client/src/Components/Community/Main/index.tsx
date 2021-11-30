097dae7e47b259df847374620d7809cc5056efac
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-11-30 19:05:16 +0900

    Feat: 뒤로가기 버튼 Common 폴더에 구현

--- client/src/Components/Community/Main/index.tsx
+++ client/src/Components/Community/Main/index.tsx
@@ -1,7 +1,7 @@
 import 'Components/Community/scss/Main.scss';
 import Logo from 'Assets/logo.png';
+import Back from 'Components/Common/Back';
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 import { CommunityApi } from 'API/CommunityApi';
 import { useState } from 'react';
@@ -9,7 +9,6 @@ import { useState } from 'react';
 export default function Community(): JSX.Element {
   const [test, setTest] = useState('');
   const isLoggedin = sessionStorage.getItem('access_token');
-  const navigate = useNavigate();
   const CustomToastWithLink = () => (
     <div>
       <Link style={{ textDecoration: 'none', color: 'gray' }} to="/login">
@@ -33,6 +32,7 @@ export default function Community(): JSX.Element {
   return (
     <section className="Main__Container">
       <div className="Main__Title">
+        <Back />
         <img className="Main__Logo-Mobile" src={Logo} alt="로고" />
         <h1>당신의 이야기를 들려주세요.</h1>
         <h3>이 곳은 여러분의 식물과 관련된 이야기를 쓰는 곳입니다.</h3>
