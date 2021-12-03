1e7e3f9d87f3fe477b3676b887bc932dabd64174
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-12-03 12:21:55 +0900

    Fix: 네비게이션바 수정

--- client/src/Components/Home/Second.tsx
+++ client/src/Components/Home/Second.tsx
@@ -3,9 +3,9 @@ import Video from 'Assets/Home_Second.mp4';
 export default function Second(): JSX.Element {
   return (
     <section className="Second__Container">
-      <h1>
-        Enjoy your PLAN IT life!<h2>+</h2>
-      </h1>
+      <span>
+        Enjoy your PLAN IT life!<div>+</div>
+      </span>
       <video autoPlay muted loop playsInline>
         <source src={Video} type="video/mp4" />
       </video>
