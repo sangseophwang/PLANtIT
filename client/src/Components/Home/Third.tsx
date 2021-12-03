47d729033b7c4ae9340db7556080dcc7713833cb
JinKyung Kim <kimradium@naver.com>
2021-12-03 15:13:07 +0900

    fix: 홈페이지 3번째 질병도감 검색 기능 추가(디자인x)
    Former-commit-id: 6fc1e2edd07f5e96d46c0115fdb47f8ff0549847

--- client/src/Components/Home/Third.tsx
+++ client/src/Components/Home/Third.tsx
@@ -1,23 +1,99 @@
 import 'Components/Home/scss/Third.scss';
-import Test from 'Assets/test.jpeg';
-export default function Third(): JSX.Element {
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Third(props: any): JSX.Element {
+  const [filterData, setFilterData] = useState<any[]>([]);
+  const [clearWord, setClearWord] = useState('');
+
+  const FilterOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchWord = e.target.value;
+    setClearWord(searchWord);
+    const newFilter = props.data.filter((value: any) => {
+      return value.name.includes(searchWord);
+    });
+    setFilterData(newFilter);
+  };
+
+  const clearInput = () => {
+    setFilterData([]);
+    setClearWord('');
+  };
+
+  useEffect(() => {}, [props]);
+
   return (
     <section className="Third__Container">
-      <img className="Third__Background" src={Test} alt="테스트 이미지" />
-      <div className="Third__Contents">
-        <div className="Third__Image">
-          <img src={Test} alt="테스트 이미지" />
+      <section className="HSearch__Container">
+        <div className="HSearch__Input">
+          {filterData.length === 0 ? (
+            <svg
+              className="HSearch__Icon"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="HClose__Icon"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              onClick={clearInput}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          )}
+          <input
+            type="text"
+            value={clearWord}
+            placeholder="작물의 이름을 검색해주세요"
+            onChange={FilterOnChangeHandler}
+          />
         </div>
-        <div className="Third__Text">
-          <h1>for your plant,</h1>
-          <h1>for our planet</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quos tempora quia dolore, accusamus error necessitatibus repellendus eum recusandae officiis assumenda aliquid maiores
-            similique odit! Aut excepturi quis quia fugiat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quos tempora quia dolore, accusamus error necessitatibus repellendus eum
-            recusandae officiis assumenda aliquid maiores similique odit! Aut excepturi quis quia fugiat?
-          </p>
+      </section>
+      {filterData.length !== 0 && (
+        <div className="Data__Result">
+          {filterData.map(option => (
+            <Link
+              to={`/Dictionary?name=${option.name}`}
+              style={{ textDecoration: 'none', color: 'black' }}
+            >
+              <div className="Data__Item">
+                <svg
+                  className="Search__Sub-Icon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+                {option.name}
+              </div>
+            </Link>
+          ))}
         </div>
-      </div>
+      )}
     </section>
   );
 }
