88a18a174cf8993ef623c33f45f223593dda9e49
JinKyung Kim <kimradium@naver.com>
2021-12-05 20:15:22 +0900

    fix: Exception 문구 수정
    Former-commit-id: 75908402959720824318dd5eb76be84177500de8

--- client/src/Components/Result/Exception.tsx
+++ client/src/Components/Result/Exception.tsx
@@ -5,7 +5,9 @@ import { Link } from 'react-router-dom';
 const Exception = () => {
   return (
     <div className="Exception__Container">
-      <div className="Exception__Text">인식 오류가 발생하였습니다.</div>
+      <div className="Exception__Text">
+        정상이거나 올바르지 않은 사진입니다.
+      </div>
       <Link to="/analysis" className="Button__Go-analysis">
         다시 검사하기
       </Link>
