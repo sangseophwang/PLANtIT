57fcf8203f0246ac0cf3f194294ebba3f606082a
heejae <jhj967878@naver.com>
2021-12-07 23:28:47 +0900

    fix : user description default fix
    Former-commit-id: 541ee992599dbccb53e77205b264b6a325797ca6

--- server/user/models.py
+++ server/user/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     nickname = models.CharField(max_length=50)
     user_type = models.IntegerField()
     refresh_token = models.CharField(max_length=512, null=True)
-    description = models.CharField(max_length=256, null=f"안녕하세요 {nickname}입니다.")
+    description = models.CharField(max_length=256, null=True, default=f"안녕하세요 {nickname}입니다.")
     image = models.CharField(max_length=100, null=True, default='https://team3-plantit.s3.ap-northeast-2.amazonaws.com/profile/default.png') # image url
     
     def __str__(self):
